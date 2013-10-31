067f76a9a91ba6bf26f9fc07b80455da4c917e0b
Eric Malm and Onsi Fakhouri <pair+eric+onsi@pivotallabs.com>
2013-10-31 10:43:07 -0700

    WIP cassandra

---
+++ storecassandra/store_cassandra.go
@@ -0,0 +1,362 @@
+package storecassandra
+
+import (
+	"github.com/cloudfoundry/hm9000/config"
+	"github.com/cloudfoundry/hm9000/helpers/timeprovider"
+	"github.com/cloudfoundry/hm9000/models"
+	"github.com/cloudfoundry/hm9000/store"
+	"time"
+	"tux21b.org/v1/gocql"
+)
+
+type StoreCassandra struct {
+	session      *gocql.Session
+	conf         config.Config
+	timeProvider timeprovider.TimeProvider
+}
+
+func New(conf config.Config, timeProvider timeprovider.TimeProvider) (*StoreCassandra, error) {
+	s := &StoreCassandra{
+		conf:         conf,
+		timeProvider: timeProvider,
+	}
+
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.DefaultPort = 9042
+	cluster.Consistency = gocql.One
+	cluster.NumConns = 1
+	cluster.NumStreams = 1
+	var err error
+
+	s.session, err = cluster.CreateSession()
+	if err != nil {
+		println("FAILED TO CREATE SESSION")
+		return s, err
+	}
+
+	err = s.session.Query(`CREATE KEYSPACE IF NOT EXISTS hm9000 WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }`).Exec()
+	if err != nil {
+		println("FAILED TO CREATE KEYSPACE")
+		return s, err
+	}
+	s.session.Close()
+
+	cluster.Keyspace = "hm9000"
+	s.session, err = cluster.CreateSession()
+
+	err = s.session.Query(`CREATE TABLE IF NOT EXISTS DesiredStates (app_guid text, app_version text, number_of_instances int, memory int, state text, package_state text, updated_at bigint, expires bigint, PRIMARY KEY (app_guid, app_version))`).Exec()
+	if err != nil {
+		println("FAILED TO CREATE TABLE DesiredStates")
+		return s, err
+	}
+
+	err = s.session.Query(`CREATE TABLE IF NOT EXISTS PendingStartMessages (app_guid text, app_version text, message_id text, send_on bigint, sent_on bigint, keep_alive int, index_to_start int, priority double, skip_verification boolean, PRIMARY KEY (app_guid, app_version, index_to_start))`).Exec()
+	if err != nil {
+		println("FAILED TO CREATE TABLE PendingStartMessages")
+		return s, err
+	}
+
+	err = s.session.Query(`CREATE TABLE IF NOT EXISTS PendingStopMessages (app_guid text, app_version text, message_id text, send_on bigint, sent_on bigint, keep_alive int, instance_guid text, PRIMARY KEY (app_guid, app_version, instance_guid))`).Exec()
+	if err != nil {
+		println("FAILED TO CREATE TABLE PendingStopMessages")
+		return s, err
+	}
+
+	err = s.session.Query(`CREATE TABLE IF NOT EXISTS Freshness (key text, created_at bigint, expires bigint, PRIMARY KEY (key))`).Exec()
+	if err != nil {
+		println("FAILED TO CREATE TABLE Freshness")
+		return s, err
+	}
+
+	return s, err
+}
+
+func (s *StoreCassandra) SaveDesiredState(desiredStates ...models.DesiredAppState) error {
+	for _, state := range desiredStates {
+		err := s.session.Query(`INSERT INTO DesiredStates (app_guid, app_version, number_of_instances, memory, state, package_state, updated_at, expires) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`, state.AppGuid, state.AppVersion, state.NumberOfInstances, state.Memory, state.State, state.PackageState, int64(state.UpdatedAt.Unix()), s.timeProvider.Time().Unix()+int64(s.conf.DesiredStateTTL())).Exec()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *StoreCassandra) GetDesiredState() (map[string]models.DesiredAppState, error) {
+	result := map[string]models.DesiredAppState{}
+	var err error
+
+	iter := s.session.Query(`SELECT app_guid, app_version, number_of_instances, memory, state, package_state, updated_at, expires FROM DesiredStates`).Iter()
+
+	var appGuid, appVersion, state, packageState string
+	var numberOfInstances, memory int32
+	var updatedAt, expires int64
+
+	currentTime := s.timeProvider.Time().Unix()
+
+	for iter.Scan(&appGuid, &appVersion, &numberOfInstances, &memory, &state, &packageState, &updatedAt, &expires) {
+		if expires <= currentTime {
+			err = s.deleteDesiredState(appGuid, appVersion)
+			if err != nil {
+				return result, err
+			}
+		} else {
+			desiredState := models.DesiredAppState{
+				AppGuid:           appGuid,
+				AppVersion:        appVersion,
+				NumberOfInstances: int(numberOfInstances),
+				Memory:            int(memory),
+				State:             models.AppState(state),
+				PackageState:      models.AppPackageState(packageState),
+				UpdatedAt:         time.Unix(updatedAt, 0),
+			}
+			result[desiredState.StoreKey()] = desiredState
+		}
+	}
+
+	err = iter.Close()
+
+	return result, err
+}
+
+func (s *StoreCassandra) deleteDesiredState(appGuid string, appVersion string) error {
+	return s.session.Query(`DELETE FROM DesiredStates WHERE app_guid=? AND app_version=?`, appGuid, appVersion).Exec()
+}
+
+func (s *StoreCassandra) DeleteDesiredState(desiredStates ...models.DesiredAppState) error {
+	for _, state := range desiredStates {
+		err := s.deleteDesiredState(state.AppGuid, state.AppVersion)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *StoreCassandra) SavePendingStartMessages(startMessages ...models.PendingStartMessage) error {
+	for _, startMessage := range startMessages {
+		err := s.session.Query(`INSERT INTO PendingStartMessages (app_guid, app_version, message_id, send_on, sent_on, keep_alive, index_to_start, priority, skip_verification) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+			startMessage.AppGuid,
+			startMessage.AppVersion,
+			startMessage.MessageId,
+			startMessage.SendOn,
+			startMessage.SentOn,
+			startMessage.KeepAlive,
+			startMessage.IndexToStart,
+			startMessage.Priority,
+			startMessage.SkipVerification).Exec()
+		if err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+}
+
+func (s *StoreCassandra) GetPendingStartMessages() (map[string]models.PendingStartMessage, error) {
+	startMessages := map[string]models.PendingStartMessage{}
+	var err error
+
+	iter := s.session.Query(`SELECT app_guid, app_version, message_id, send_on, sent_on, keep_alive, index_to_start, priority, skip_verification FROM PendingStartMessages`).Iter()
+
+	var messageId, appGuid, appVersion string
+	var sendOn, sentOn int64
+	var keepAlive, indexToStart int
+	var priority float64
+	var skipVerification bool
+
+	for iter.Scan(&appGuid, &appVersion, &messageId, &sendOn, &sentOn, &keepAlive, &indexToStart, &priority, &skipVerification) {
+		startMessage := models.PendingStartMessage{
+			PendingMessage: models.PendingMessage{
+				MessageId:  messageId,
+				SendOn:     sendOn,
+				SentOn:     sentOn,
+				KeepAlive:  keepAlive,
+				AppGuid:    appGuid,
+				AppVersion: appVersion,
+			},
+			IndexToStart:     indexToStart,
+			Priority:         priority,
+			SkipVerification: skipVerification,
+		}
+		startMessages[startMessage.StoreKey()] = startMessage
+	}
+
+	err = iter.Close()
+
+	return startMessages, err
+}
+
+func (s *StoreCassandra) DeletePendingStartMessages(startMessages ...models.PendingStartMessage) error {
+	for _, startMessage := range startMessages {
+		err := s.session.Query(`DELETE FROM PendingStartMessages WHERE app_guid=? AND app_version=? AND index_to_start=?`, startMessage.AppGuid, startMessage.AppVersion, startMessage.IndexToStart).Exec()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *StoreCassandra) SavePendingStopMessages(stopMessages ...models.PendingStopMessage) error {
+	for _, stopMessage := range stopMessages {
+		err := s.session.Query(`INSERT INTO PendingStopMessages (app_guid, app_version, message_id, send_on, sent_on, keep_alive, instance_guid) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+			stopMessage.AppGuid,
+			stopMessage.AppVersion,
+			stopMessage.MessageId,
+			stopMessage.SendOn,
+			stopMessage.SentOn,
+			stopMessage.KeepAlive,
+			stopMessage.InstanceGuid).Exec()
+		if err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+}
+
+func (s *StoreCassandra) GetPendingStopMessages() (map[string]models.PendingStopMessage, error) {
+	stopMessages := map[string]models.PendingStopMessage{}
+	var err error
+
+	iter := s.session.Query(`SELECT app_guid, app_version, message_id, send_on, sent_on, keep_alive, instance_guid FROM PendingStopMessages`).Iter()
+
+	var messageId, appGuid, appVersion, instanceGuid string
+	var sendOn, sentOn int64
+	var keepAlive int
+
+	for iter.Scan(&appGuid, &appVersion, &messageId, &sendOn, &sentOn, &keepAlive, &instanceGuid) {
+		stopMessage := models.PendingStopMessage{
+			PendingMessage: models.PendingMessage{
+				MessageId:  messageId,
+				SendOn:     sendOn,
+				SentOn:     sentOn,
+				KeepAlive:  keepAlive,
+				AppGuid:    appGuid,
+				AppVersion: appVersion,
+			},
+			InstanceGuid: instanceGuid,
+		}
+		stopMessages[stopMessage.StoreKey()] = stopMessage
+	}
+
+	err = iter.Close()
+
+	return stopMessages, err
+}
+
+func (s *StoreCassandra) DeletePendingStopMessages(stopMessages ...models.PendingStopMessage) error {
+	for _, stopMessage := range stopMessages {
+		err := s.session.Query(`DELETE FROM PendingStopMessages WHERE app_guid=? AND app_version=? AND instance_guid=?`,
+			stopMessage.AppGuid,
+			stopMessage.AppVersion,
+			stopMessage.InstanceGuid).Exec()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *StoreCassandra) BumpDesiredFreshness(timestamp time.Time) error {
+	return s.session.Query(`INSERT INTO Freshness (key, created_at, expires) VALUES (?, ?, ?)`, s.conf.DesiredFreshnessKey, timestamp.Unix(), timestamp.Unix()+int64(s.conf.DesiredFreshnessTTL())).Exec()
+}
+
+func (s *StoreCassandra) IsDesiredStateFresh() (bool, error) {
+	var expires int64
+	err := s.session.Query(`SELECT expires FROM Freshness WHERE key=?`, s.conf.DesiredFreshnessKey).Scan(&expires)
+
+	if err == gocql.ErrNotFound {
+		return false, nil
+	}
+
+	if err != nil {
+		return false, err
+	}
+
+	if expires <= s.timeProvider.Time().Unix() {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (s *StoreCassandra) BumpActualFreshness(timestamp time.Time) error {
+	shouldBumpCreatedAt := false
+	var expires int64
+	err := s.session.Query(`SELECT expires FROM Freshness WHERE key=?`, s.conf.ActualFreshnessKey).Scan(&expires)
+
+	if err == gocql.ErrNotFound {
+		shouldBumpCreatedAt = true
+	} else if err != nil {
+		return err
+	} else if expires <= timestamp.Unix() {
+		shouldBumpCreatedAt = true
+	}
+
+	if shouldBumpCreatedAt {
+		err = s.session.Query(`INSERT INTO Freshness (key, created_at) VALUES (?, ?)`, s.conf.ActualFreshnessKey, timestamp.Unix()).Exec()
+		if err != nil {
+			return err
+		}
+	}
+
+	err = s.session.Query(`INSERT INTO Freshness (key, expires) VALUES (?, ?)`, s.conf.ActualFreshnessKey, timestamp.Unix()+int64(s.conf.ActualFreshnessTTL())).Exec()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *StoreCassandra) IsActualStateFresh(timestamp time.Time) (bool, error) {
+	var createdAt, expires int64
+	err := s.session.Query(`SELECT created_at, expires FROM Freshness WHERE key=?`, s.conf.ActualFreshnessKey).Scan(&createdAt, &expires)
+
+	if err == gocql.ErrNotFound {
+		return false, nil
+	}
+
+	if err != nil {
+		return false, err
+	}
+
+	currentTime := s.timeProvider.Time().Unix()
+
+	if createdAt+int64(s.conf.ActualFreshnessTTL()) <= currentTime && currentTime < expires {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (s *StoreCassandra) VerifyFreshness(time time.Time) error {
+	desiredFresh, err := s.IsDesiredStateFresh()
+	if err != nil {
+		return err
+	}
+
+	actualFresh, err := s.IsActualStateFresh(time)
+	if err != nil {
+		return err
+	}
+
+	if !desiredFresh && !actualFresh {
+		return store.ActualAndDesiredAreNotFreshError
+	}
+
+	if !desiredFresh {
+		return store.DesiredIsNotFreshError
+	}
+
+	if !actualFresh {
+		return store.ActualIsNotFreshError
+	}
+
+	return nil
+}
---
+++ storecassandra/storecassandra_test.go
@@ -0,0 +1,422 @@
+package storecassandra_test
+
+import (
+	"github.com/cloudfoundry/hm9000/config"
+	"github.com/cloudfoundry/hm9000/models"
+	storepackage "github.com/cloudfoundry/hm9000/store"
+	. "github.com/cloudfoundry/hm9000/storecassandra"
+	"github.com/cloudfoundry/hm9000/testhelpers/appfixture"
+	. "github.com/cloudfoundry/hm9000/testhelpers/custommatchers"
+	"github.com/cloudfoundry/hm9000/testhelpers/faketimeprovider"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"time"
+	"tux21b.org/v1/gocql"
+)
+
+//this should move to a store runner eventually
+func ResetCassandra() {
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.DefaultPort = 9042
+	cluster.Consistency = gocql.One
+	session, err := cluster.CreateSession()
+	Ω(err).ShouldNot(HaveOccured())
+	defer session.Close()
+	err = session.Query(`DROP KEYSPACE IF EXISTS hm9000`).Exec()
+	Ω(err).ShouldNot(HaveOccured())
+}
+
+var _ = Describe("Storecassandra", func() {
+	var store *StoreCassandra
+	var timeProvider *faketimeprovider.FakeTimeProvider
+	var app1 appfixture.AppFixture
+	var app2 appfixture.AppFixture
+
+	var startMessage1 models.PendingStartMessage
+	var startMessage2 models.PendingStartMessage
+
+	var stopMessage1 models.PendingStopMessage
+	var stopMessage2 models.PendingStopMessage
+
+	conf, _ := config.DefaultConfig()
+
+	BeforeEach(func() {
+		ResetCassandra()
+
+		timeProvider = &faketimeprovider.FakeTimeProvider{
+			TimeToProvide: time.Unix(100, 0),
+		}
+
+		var err error
+		store, err = New(conf, timeProvider)
+		Ω(err).ShouldNot(HaveOccured())
+
+		app1 = appfixture.NewAppFixture()
+		app2 = appfixture.NewAppFixture()
+	})
+
+	XMeasure("Quick", func(b Benchmarker) {
+		n := 10000
+		desired := []models.DesiredAppState{}
+		for i := 0; i < n; i++ {
+			app := appfixture.NewAppFixture()
+			desired = append(desired, app.DesiredState(1))
+		}
+
+		b.Time("WRITE", func() {
+			err := store.SaveDesiredState(desired...)
+			Ω(err).ShouldNot(HaveOccured())
+		})
+
+		b.Time("READ", func() {
+			state, err := store.GetDesiredState()
+			Ω(err).ShouldNot(HaveOccured())
+			Ω(state).Should(HaveLen(n))
+		})
+	}, 5)
+
+	Describe("Desired State", func() {
+		Describe("Writing and reading desired state", func() {
+			BeforeEach(func() {
+				err := store.SaveDesiredState(app1.DesiredState(1), app2.DesiredState(3))
+				Ω(err).ShouldNot(HaveOccured())
+			})
+
+			It("should return the stored desired state", func() {
+				state, err := store.GetDesiredState()
+				Ω(err).ShouldNot(HaveOccured())
+				Ω(state).Should(HaveLen(2))
+
+				Ω(state[app1.DesiredState(1).StoreKey()]).Should(EqualDesiredState(app1.DesiredState(1)))
+				Ω(state[app2.DesiredState(3).StoreKey()]).Should(EqualDesiredState(app2.DesiredState(3)))
+			})
+
+			Context("when the TTL expires", func() {
+				BeforeEach(func() {
+					timeProvider.TimeToProvide = time.Unix(100+int64(conf.DesiredStateTTL()), 0)
+				})
+
+				It("should expire the nodes appropriately", func() {
+					state, err := store.GetDesiredState()
+					Ω(err).ShouldNot(HaveOccured())
+					Ω(state).Should(HaveLen(0))
+				})
+			})
+
+			Describe("Updating desired state", func() {
+				BeforeEach(func() {
+					timeProvider.TimeToProvide = time.Unix(100+int64(conf.DesiredStateTTL())-10, 0)
+
+					err := store.SaveDesiredState(app2.DesiredState(2))
+					Ω(err).ShouldNot(HaveOccured())
+				})
+
+				It("should update the correct entry", func() {
+					state, err := store.GetDesiredState()
+					Ω(err).ShouldNot(HaveOccured())
+					Ω(state).Should(HaveLen(2))
+
+					Ω(state[app1.DesiredState(1).StoreKey()]).Should(EqualDesiredState(app1.DesiredState(1)))
+					Ω(state[app2.DesiredState(2).StoreKey()]).Should(EqualDesiredState(app2.DesiredState(2)))
+				})
+
+				It("should bump the TTL", func() {
+					timeProvider.TimeToProvide = time.Unix(100+int64(conf.DesiredStateTTL()), 0)
+					state, err := store.GetDesiredState()
+					Ω(err).ShouldNot(HaveOccured())
+					Ω(state).Should(HaveLen(1))
+					Ω(state[app2.DesiredState(2).StoreKey()]).Should(EqualDesiredState(app2.DesiredState(2)))
+				})
+			})
+
+			Describe("Deleting desired state", func() {
+				It("should delete the specified app but leave the others", func() {
+					err := store.DeleteDesiredState(app1.DesiredState(1))
+					Ω(err).ShouldNot(HaveOccured())
+
+					state, err := store.GetDesiredState()
+					Ω(err).ShouldNot(HaveOccured())
+					Ω(state).Should(HaveLen(1))
+					Ω(state[app2.DesiredState(3).StoreKey()]).Should(EqualDesiredState(app2.DesiredState(3)))
+				})
+			})
+		})
+
+		//can put multiple desireds in and get them all out
+		//can put multiple desireds in, delete some of them, and have the correct subset left
+		//we should be able to updated a desired
+		//test TTL behavior (creating a node, and updating a node)
+	})
+
+	Describe("Pending Start Messages", func() {
+		BeforeEach(func() {
+			startMessage1 = models.NewPendingStartMessage(timeProvider.Time(), 10, 4, "ABC", "123", 1, 1.0)
+			startMessage2 = models.NewPendingStartMessage(timeProvider.Time(), 10, 4, "DEF", "456", 1, 1.0)
+		})
+
+		Describe("Writing and reading pending start messages", func() {
+			BeforeEach(func() {
+				err := store.SavePendingStartMessages(startMessage1, startMessage2)
+				Ω(err).ShouldNot(HaveOccured())
+			})
+
+			It("should return the pending start messages", func() {
+				messages, err := store.GetPendingStartMessages()
+				Ω(err).ShouldNot(HaveOccured())
+				Ω(messages).Should(HaveLen(2))
+				Ω(messages[startMessage1.StoreKey()]).Should(Equal(startMessage1))
+				Ω(messages[startMessage2.StoreKey()]).Should(Equal(startMessage2))
+			})
+
+			Describe("Updating pending start messages", func() {
+				It("should update the correct message", func() {
+					startMessage2.Priority = 0.7
+					err := store.SavePendingStartMessages(startMessage2)
+					Ω(err).ShouldNot(HaveOccured())
+
+					messages, err := store.GetPendingStartMessages()
+					Ω(err).ShouldNot(HaveOccured())
+					Ω(messages).Should(HaveLen(2))
+					Ω(messages[startMessage1.StoreKey()]).Should(Equal(startMessage1))
+					Ω(messages[startMessage2.StoreKey()]).Should(Equal(startMessage2))
+
+				})
+			})
+
+			Describe("Deleting pending start messages", func() {
+				It("should delete the specified message but not the others", func() {
+					err := store.DeletePendingStartMessages(startMessage1)
+					Ω(err).ShouldNot(HaveOccured())
+
+					messages, err := store.GetPendingStartMessages()
+					Ω(err).ShouldNot(HaveOccured())
+					Ω(messages).Should(HaveLen(1))
+					Ω(messages[startMessage2.StoreKey()]).Should(Equal(startMessage2))
+				})
+			})
+		})
+	})
+
+	Describe("Pending Stop Messages", func() {
+		BeforeEach(func() {
+			stopMessage1 = models.NewPendingStopMessage(timeProvider.Time(), 10, 4, "ABC", "123", "XYZ")
+			stopMessage2 = models.NewPendingStopMessage(timeProvider.Time(), 10, 4, "DEF", "456", "ALPHA")
+		})
+
+		Describe("Writing and reading pending stop messages", func() {
+			BeforeEach(func() {
+				err := store.SavePendingStopMessages(stopMessage1, stopMessage2)
+				Ω(err).ShouldNot(HaveOccured())
+			})
+
+			It("should return the pending stop messages", func() {
+				messages, err := store.GetPendingStopMessages()
+				Ω(err).ShouldNot(HaveOccured())
+				Ω(messages).Should(HaveLen(2))
+				Ω(messages[stopMessage1.StoreKey()]).Should(Equal(stopMessage1))
+				Ω(messages[stopMessage2.StoreKey()]).Should(Equal(stopMessage2))
+			})
+
+			Describe("Updating pending stop messages", func() {
+				It("should update the correct message", func() {
+					stopMessage2.SendOn += 10
+					err := store.SavePendingStopMessages(stopMessage2)
+					Ω(err).ShouldNot(HaveOccured())
+
+					messages, err := store.GetPendingStopMessages()
+					Ω(err).ShouldNot(HaveOccured())
+					Ω(messages).Should(HaveLen(2))
+					Ω(messages[stopMessage1.StoreKey()]).Should(Equal(stopMessage1))
+					Ω(messages[stopMessage2.StoreKey()]).Should(Equal(stopMessage2))
+
+				})
+			})
+
+			Describe("Deleting pending stop messages", func() {
+				It("should delete the specified message but not the others", func() {
+					err := store.DeletePendingStopMessages(stopMessage1)
+					Ω(err).ShouldNot(HaveOccured())
+
+					messages, err := store.GetPendingStopMessages()
+					Ω(err).ShouldNot(HaveOccured())
+					Ω(messages).Should(HaveLen(1))
+					Ω(messages[stopMessage2.StoreKey()]).Should(Equal(stopMessage2))
+				})
+			})
+		})
+	})
+
+	Describe("Freshness", func() {
+		Describe("Desired freshness", func() {
+			Context("when the desired freshness is missing", func() {
+				Context("and we bump the freshnesss", func() {
+					BeforeEach(func() {
+						err := store.BumpDesiredFreshness(timeProvider.Time())
+						Ω(err).ShouldNot(HaveOccured())
+					})
+
+					It("should mark the desired state as fresh", func() {
+						isFresh, err := store.IsDesiredStateFresh()
+						Ω(err).ShouldNot(HaveOccured())
+						Ω(isFresh).Should(BeTrue())
+					})
+
+					Context("when the desired state TTL expires", func() {
+						BeforeEach(func() {
+							timeProvider.IncrementBySeconds(conf.DesiredFreshnessTTL())
+						})
+
+						It("should no longer be fresh", func() {
+							isFresh, err := store.IsDesiredStateFresh()
+							Ω(err).ShouldNot(HaveOccured())
+							Ω(isFresh).Should(BeFalse())
+						})
+					})
+				})
+
+				It("should not be fresh", func() {
+					isFresh, err := store.IsDesiredStateFresh()
+					Ω(err).ShouldNot(HaveOccured())
+					Ω(isFresh).Should(BeFalse())
+				})
+			})
+
+			Context("when the desired freshness is present", func() {
+				BeforeEach(func() {
+					timeProvider.IncrementBySeconds(10)
+					err := store.BumpDesiredFreshness(timeProvider.Time())
+					Ω(err).ShouldNot(HaveOccured())
+
+				})
+
+				It("should bump the ttl", func() {
+					timeProvider.IncrementBySeconds(conf.DesiredFreshnessTTL() - 10)
+
+					isFresh, err := store.IsDesiredStateFresh()
+					Ω(err).ShouldNot(HaveOccured())
+					Ω(isFresh).Should(BeTrue())
+				})
+
+				It("should expire after the new ttl expires", func() {
+					timeProvider.IncrementBySeconds(conf.DesiredFreshnessTTL())
+
+					isFresh, err := store.IsDesiredStateFresh()
+					Ω(err).ShouldNot(HaveOccured())
+					Ω(isFresh).Should(BeFalse())
+				})
+			})
+		})
+
+		Describe("Bumping actual freshness", func() {
+			Context("when the actual freshness is missing", func() {
+				Context("and we bump the freshnesss", func() {
+					BeforeEach(func() {
+						err := store.BumpActualFreshness(timeProvider.Time())
+						Ω(err).ShouldNot(HaveOccured())
+					})
+
+					It("should not report the actual state as fresh", func() {
+						isFresh, err := store.IsActualStateFresh(timeProvider.Time())
+						Ω(err).ShouldNot(HaveOccured())
+						Ω(isFresh).Should(BeFalse())
+					})
+
+					Context("when we bump the freshness again before expiry", func() {
+						BeforeEach(func() {
+							timeProvider.IncrementBySeconds(10)
+							err := store.BumpActualFreshness(timeProvider.Time())
+							Ω(err).ShouldNot(HaveOccured())
+							timeProvider.IncrementBySeconds(conf.ActualFreshnessTTL() - 10)
+						})
+
+						It("should be fresh", func() {
+							isFresh, err := store.IsActualStateFresh(timeProvider.Time())
+							Ω(err).ShouldNot(HaveOccured())
+							Ω(isFresh).Should(BeTrue())
+						})
+
+						Context("when we run past expiration time", func() {
+							BeforeEach(func() {
+								timeProvider.IncrementBySeconds(10)
+							})
+
+							It("should no longer be fresh", func() {
+								isFresh, err := store.IsActualStateFresh(timeProvider.Time())
+								Ω(err).ShouldNot(HaveOccured())
+								Ω(isFresh).Should(BeFalse())
+							})
+
+							Context("When we start bumping freshness again", func() {
+								BeforeEach(func() {
+									err := store.BumpActualFreshness(timeProvider.Time())
+									Ω(err).ShouldNot(HaveOccured())
+									timeProvider.IncrementBySeconds(10)
+									err = store.BumpActualFreshness(timeProvider.Time())
+									Ω(err).ShouldNot(HaveOccured())
+									timeProvider.IncrementBySeconds(conf.ActualFreshnessTTL() - 10)
+								})
+
+								It("should become fresh again", func() {
+									isFresh, err := store.IsActualStateFresh(timeProvider.Time())
+									Ω(err).ShouldNot(HaveOccured())
+									Ω(isFresh).Should(BeTrue())
+								})
+							})
+						})
+					})
+				})
+
+				It("should not be fresh", func() {
+					isFresh, err := store.IsActualStateFresh(timeProvider.Time())
+					Ω(err).ShouldNot(HaveOccured())
+					Ω(isFresh).Should(BeFalse())
+				})
+			})
+		})
+
+		Describe("VerifyFreshness", func() {
+			Context("when both desired and actual are not fresh", func() {
+				It("should return the correct error", func() {
+					Ω(store.VerifyFreshness(timeProvider.Time())).Should(Equal(storepackage.ActualAndDesiredAreNotFreshError))
+				})
+			})
+
+			Context("when only desired is fresh", func() {
+				BeforeEach(func() {
+					store.BumpDesiredFreshness(timeProvider.Time())
+				})
+
+				It("should return the correct error", func() {
+					Ω(store.VerifyFreshness(timeProvider.Time())).Should(Equal(storepackage.ActualIsNotFreshError))
+				})
+			})
+
+			Context("when only actual is fresh", func() {
+				BeforeEach(func() {
+					store.BumpActualFreshness(timeProvider.Time())
+					timeProvider.IncrementBySeconds(10)
+					store.BumpActualFreshness(timeProvider.Time())
+					timeProvider.IncrementBySeconds(conf.ActualFreshnessTTL() - 10)
+				})
+
+				It("should return the correct error", func() {
+					Ω(store.VerifyFreshness(timeProvider.Time())).Should(Equal(storepackage.DesiredIsNotFreshError))
+				})
+			})
+
+			Context("when both desired and actual are fresh", func() {
+				BeforeEach(func() {
+					store.BumpDesiredFreshness(timeProvider.Time())
+					store.BumpActualFreshness(timeProvider.Time())
+					timeProvider.IncrementBySeconds(10)
+					store.BumpActualFreshness(timeProvider.Time())
+					timeProvider.IncrementBySeconds(conf.ActualFreshnessTTL() - 10)
+				})
+
+				It("should not error", func() {
+					Ω(store.VerifyFreshness(timeProvider.Time())).Should(BeNil())
+				})
+			})
+		})
+	})
+})
